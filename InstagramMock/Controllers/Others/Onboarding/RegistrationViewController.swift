0e304cad8500f8832b257ea24a4e94bf4920db12
Koki <koki.111.sui@gmail.com>
2022-01-18 16:24:01 +0100

    add registration form

--- InstagramMock/Controllers/Others/Onboarding/RegistrationViewController.swift
+++ InstagramMock/Controllers/Others/Onboarding/RegistrationViewController.swift
@@ -69,18 +69,63 @@ class RegistrationViewController: UIViewController {
         button.setTitle("Sign up", for: .normal)
         button.layer.masksToBounds = true
         button.layer.cornerRadius = Constants.cornerRadius
-        button.backgroundColor = .systemBlue
+        button.backgroundColor = .systemGreen
         button.setTitleColor(.white, for: .normal)
         return button
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        registerButton.addTarget(self, action: #selector(didTapRegister), for: .touchUpInside)
+        usernameField.delegate = self
+        emailField.delegate = self
+        passwordField.delegate = self
+        view.addSubview(usernameField)
+        view.addSubview(emailField)
+        view.addSubview(passwordField)
+        view.addSubview(registerButton)
 
         // Do any additional setup after loading the view.
         view.backgroundColor = .systemBackground
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        usernameField.frame = CGRect(x: 20, y: view.safeAreaInsets.top + 100, width: view.width - 40, height: 52)
+        emailField.frame = CGRect(x: 20, y: usernameField.bottom + 10, width: view.width - 40, height: 52)
+        passwordField.frame = CGRect(x: 20, y: emailField.bottom + 10, width: view.width - 40, height: 52)
+        registerButton.frame = CGRect(x: 20, y: passwordField.bottom + 10, width: view.width - 40, height: 52)
 
+    }
+    
+    @objc func didTapRegister () {
+        usernameField.resignFirstResponder()
+        emailField.resignFirstResponder()
+        passwordField.resignFirstResponder()
+        
+        guard let email = emailField.text, !email.isEmpty,
+              let password = passwordField.text, !password.isEmpty, password.count >= 8,
+              let username = usernameField.text, !username.isEmpty
+        else {
+                  return
+              }
 
+    }
+}
+
+extension RegistrationViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == usernameField {
+            emailField.becomeFirstResponder()
+        }
+        else if textField == emailField {
+            passwordField.becomeFirstResponder()
+        }
+        else {
+            didTapRegister()
+        }
+        
+        return true
+    }
 }
